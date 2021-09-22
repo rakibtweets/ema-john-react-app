0a8afa53f4c5f2b6cc6cd6f244aed8c9b62c3a90
rakibtweets <rakibhasanroky0@gmail.com>
2021-09-23 01:59:43 +0600

    header component done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img className="logo" src={logo} alt="Brand-logo" />
+      <nav className="nav-items d-flex bg-dark p-2  ">
+        <a className="nav-link" href="/shop">
+          Shop
+        </a>
+        <a className="nav-link" href="/oders">
+          Order Reviews
+        </a>
+        <a className="nav-link" href="/inventory">
+          Manage Inventory
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
